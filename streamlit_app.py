3d29394ac3f1e47b0e90194967194c5ff828ebec
endicot <73353977+endicot@users.noreply.github.com>
2026-08-04 16:16:11 -0400

    Update streamlit_app.py
    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 #debug verify key
 st.write(st.secrets["connections"]["snowflake"]["test_key"])
-st.write(st.secrets["connections"]["snowflake"]["private_key"]
+st.write(st.secrets["connections"]["snowflake"]["private_key"])
 # st.secrets["connections"]["snowflake"]["private_key"]
 # st.write(list(st.secrets["connections"]["snowflake"].keys()))
 
