60e65fef8ba86b19a60d26e836233e3c7d413a67
endicot <73353977+endicot@users.noreply.github.com>
2026-08-11 16:57:50 -0400

    Add SEARCH_ON column to fruit_options selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ session = Session.builder.configs({
     "schema": st.secrets["connections"]["snowflake"]["schema"],
 }).create()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
